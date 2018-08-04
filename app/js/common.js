b60ed13817e48c4c8fdf0ab2acb36f90c57bb68d
Nik Razumkov <nik.razumkov@gmail.com>
2018-08-04 20:32:27 +0300

    add asaid callback

--- app/js/common.js
+++ app/js/common.js
@@ -4,6 +4,20 @@ $(function() {
 		auto: true
 	 });
 
+/* Заказать обратный звонок*/
+   $('.bcClicker').on('click', function(){
+		$(this).hide();
+		$('.bcForm').fadeToggle();
+
+		$('.bcForm').addClass("flexy");
+	});
+
+	$('.bcForm .fa-times-circle').on('click', function(){
+		$('.bcClicker').show();
+		$('.bcForm').removeClass("flex");
+		$('.bcForm').hide();
+	});
+/* END Заказать обратный звонок*/
 
 
 });
